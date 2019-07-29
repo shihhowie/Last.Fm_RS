d60006c6342d3ec0c5b2763c806e3fc40281717a
Howard <howardshihhoward@gmail.com>
2019-07-29 11:55:05 -0700

    Adding description

--- DataRetrieval.py
+++ DataRetrieval.py
@@ -11,7 +11,7 @@ API_KEY = "92e5065a366659b3cb6940ae714bb41f"
 API_SECRET = "9b95e86fe7e4ff114cb2622889f1e37e"
 username = "thehowieman"
 
-password_hash = pylast.md5("h#11122")
+password_hash = pylast.md5("12345")
 
 network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
                                username=username, password_hash=password_hash)
